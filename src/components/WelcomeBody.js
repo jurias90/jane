1e7fe738748418c78b8e1c615bfd601cd94f3208
dantails <jesus.urias90@gmail.com>
2019-11-11 18:25:17 -0800

    Fixing UI

--- src/components/WelcomeBody.js
+++ src/components/WelcomeBody.js
@@ -1,33 +1,35 @@
-import React from "react";
-import styled from "@emotion/styled";
-import { Link } from "react-router-dom"
+import React from 'react'
+import styled from '@emotion/styled'
+import { Link } from 'react-router-dom'
 
 const Welcome = styled.div({
-  textAlign: "center",
-  height: "calc(100vh - 70px)",
-  display: "inline-block",
-  alignItems: "center",
-  padding: "23%",
-  textShadow: "1px 2px #c0c0c0"
-});
+  textAlign: 'center',
+  height: 'calc(100vh - 70px)',
+  display: 'inline-block',
+  alignItems: 'center',
+  padding: 'auto 20%',
+  textShadow: '1px 2px #c0c0c0',
+})
 const Continue = styled.button({
-  backgroundColor: "#66ff33",
-  textAlign: "center",
-  borderRadius: "15px",
-  width: "10em",
-  height: "2em",
-  marginTop: "5%",
-  fontSize: "90%",
-  boxShadow: "1px 2px"
-});
+  backgroundColor: '#66ff33',
+  textAlign: 'center',
+  borderRadius: '15px',
+  width: '10em',
+  height: '2em',
+  marginTop: '5%',
+  fontSize: '90%',
+  boxShadow: '1px 2px',
+})
 
 function WelcomeBody() {
   return (
     <Welcome>
       <h1>Discover the best in cannabis at Jane.</h1>
-      <Link to="/subscribe"><Continue onClick>Continue</Continue></Link>
+      <Link to="/subscribe">
+        <Continue onClick>Continue</Continue>
+      </Link>
     </Welcome>
-  );
+  )
 }
 
-export default WelcomeBody;
+export default WelcomeBody
